122a3f815946dd25be1441389ea78a4db9d93ea5
sdh <it.s.aemt@gmail.com>
2021-09-15 22:28:52 +0900

    Init

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FieldOfView : MonoBehaviour {
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+}
